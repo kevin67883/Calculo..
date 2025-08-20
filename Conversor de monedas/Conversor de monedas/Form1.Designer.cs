6b88b9bf1b167c2905523e786e7036b0d74476bf
Kevin Manrique Campos Granados <USSS027225@ugb.edu.sv>
2025-08-20 17:27:30 -0600

    Conversor de Monedas

---
+++ Conversor de monedas/Conversor de monedas/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Conversor_de_monedas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnConvertir = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.cboConvertirA = new System.Windows.Forms.ComboBox();
+            this.cboConvertirDe = new System.Windows.Forms.ComboBox();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.txtRespuesta = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(276, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Conversion de Monedas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(126, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingrese cantidad:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(144, 193);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label3.Size = new System.Drawing.Size(98, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Convertir de:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(432, 193);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "A:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(223, 275);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Equivale a:";
+            // 
+            // btnConvertir
+            // 
+            this.btnConvertir.Location = new System.Drawing.Point(280, 344);
+            this.btnConvertir.Name = "btnConvertir";
+            this.btnConvertir.Size = new System.Drawing.Size(102, 28);
+            this.btnConvertir.TabIndex = 5;
+            this.btnConvertir.Text = "Convertir";
+            this.btnConvertir.UseVisualStyleBackColor = true;
+            this.btnConvertir.Click += new System.EventHandler(this.btnConvertir_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(463, 349);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(96, 23);
+            this.btnSalir.TabIndex = 6;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // cboConvertirA
+            // 
+            this.cboConvertirA.FormattingEnabled = true;
+            this.cboConvertirA.Items.AddRange(new object[] {
+            "Dolar",
+            "Euro",
+            "Quetzal",
+            "Libras Esterlinas",
+            "Yen Japones",
+            "Colón Salvadoreño",
+            "Lempira",
+            "Peso Méxicano",
+            "Cordóba",
+            "Soles"});
+            this.cboConvertirA.Location = new System.Drawing.Point(498, 195);
+            this.cboConvertirA.Name = "cboConvertirA";
+            this.cboConvertirA.Size = new System.Drawing.Size(121, 21);
+            this.cboConvertirA.TabIndex = 7;
+            // 
+            // cboConvertirDe
+            // 
+            this.cboConvertirDe.FormattingEnabled = true;
+            this.cboConvertirDe.Items.AddRange(new object[] {
+            "Dolar",
+            "Euro",
+            "Quetzal",
+            "Libras Esterlinas",
+            "Yen Japones",
+            "Colón Salvadoreño",
+            "Lempiras",
+            "Peso Méxicano",
+            "Córdoba",
+            "Soles"});
+            this.cboConvertirDe.Location = new System.Drawing.Point(273, 191);
+            this.cboConvertirDe.Name = "cboConvertirDe";
+            this.cboConvertirDe.Size = new System.Drawing.Size(121, 21);
+            this.cboConvertirDe.TabIndex = 8;
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(319, 124);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(100, 20);
+            this.txtCantidad.TabIndex = 9;
+            this.txtCantidad.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtRespuesta
+            // 
+            this.txtRespuesta.Location = new System.Drawing.Point(335, 277);
+            this.txtRespuesta.Name = "txtRespuesta";
+            this.txtRespuesta.Size = new System.Drawing.Size(100, 20);
+            this.txtRespuesta.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtRespuesta);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.cboConvertirDe);
+            this.Controls.Add(this.cboConvertirA);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnConvertir);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnConvertir;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.ComboBox cboConvertirA;
+        private System.Windows.Forms.ComboBox cboConvertirDe;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.TextBox txtRespuesta;
+    }
+}
+
---
+++ Conversor de monedas/Conversor de monedas/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Conversor_de_monedas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvertir_Click(object sender, EventArgs e)
+        {
+            Double convertir = 1, convertido = 1, Resultado = 0, Cantidad = 0;
+            Double Dolar = 1.00, Euro = 0.92, Quetzal = 7.87, LbrEster = 0.78,
+            YenJapon = 156.25, ColonSalva = 8.75, Lempira = 24.60,
+            PesoMex = 19.20, Cordoba = 36.50, Soles = 3.70;
+
+            if (!Double.TryParse(txtCantidad.Text, out Cantidad))
+            {
+                MessageBox.Show("Ingrese solo números.");
+                return;
+            }
+
+
+            switch (cboConvertirDe.SelectedIndex)
+
+            {
+                case 0: convertir = Dolar; break;
+
+                case 1: convertir = Euro; break;
+
+                case 2: convertir = Quetzal; break;
+
+                case 3: convertir = LbrEster; break;
+
+                case 4: convertir = YenJapon; break;
+
+                case 5: convertir = ColonSalva; break;
+
+                case 6: convertir = Lempira; break;
+
+                case 7: convertir = PesoMex; break;
+
+                case 8: convertir = Cordoba; break;
+
+                case 9: convertir = Soles; break;
+
+                default: convertir = 1; break;
+            }
+
+            switch (cboConvertirA.SelectedIndex)
+
+            {
+                case 0: convertido = Dolar; break;
+
+                case 1: convertido = Euro; break;
+
+                case 2: convertido = Quetzal; break;
+
+                case 3: convertido = LbrEster; break;
+
+                case 4: convertido = YenJapon; break;
+
+                case 5: convertido = ColonSalva; break;
+
+                case 6: convertido = Lempira; break;
+
+                case 7: convertido = PesoMex; break;
+
+                case 8: convertido = Cordoba; break;
+
+                case 9: convertido = Soles; break;
+
+                default: convertido = 1; break;
+            }
+
+            Resultado = Cantidad * (convertido / convertir);
+
+            txtRespuesta.Text = Resultado.ToString("F2");
+
+
+
+        }
+
+        private void btn2Salir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+    }
+
